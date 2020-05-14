93948f8f534d28e9b80aa3eaeaca348ccae4e762
marcin_bohm <57274664+marcinbohm@users.noreply.github.com>
2020-05-14 05:47:04 +0200

    Add files via upload

---
+++ someexercises.js
@@ -0,0 +1,43 @@
+/* Functions */
+
+const sum = (a) => (b) => a + b
+const sum2 = (a, b) => a + b
+Array.prototype.duplicate = function() {
+    return this.concat(this)
+}
+var string = 'This make I am written in reverse'
+.split('').reverse().join('');
+function asynchrony() {
+    console.log('one');
+    setTimeout(function() {
+        console.log('two');
+    }, 0)
+    console.log('three');
+}
+
+function hello() {
+    if ([]) {
+        console.log('hello'); // It will work
+      }
+}
+
+function reverseloop(){
+    for (var i = 0; i < 10; i++) {
+        console.log(9-i)
+    }
+}
+
+/* Console output */
+
+console.log(sum(1)(2)) // 3
+console.log(sum2(1, 2)) // 3
+console.log([1, 2, 3].duplicate()) // [1, 2, 3, 1, 2, 3]
+console.log(3+"3") // 33
+console.log(string) // esrever ni nettirw ma I ekam sihT
+console.log(asynchrony()) // one three two( two will be always after other console logs because its in WEB API, then in message queue)
+console.log(hello()) // hello
+console.log(reverseloop()) // 9 8 7 6 5 4 3 2 1
+
+/* Some curio in JS */
+
+// if we alert([1, 2, 3] == [1, 2, 3]) we will get false in return but why? The answer is simple - converted [1, 2, 3] array becomes "1, 2, 3" so [1, 2, 3] == "1, 2, 3" is true
